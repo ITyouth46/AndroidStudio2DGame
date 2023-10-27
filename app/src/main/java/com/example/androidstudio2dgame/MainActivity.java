4aa083928d0732bf354d3962d99433aab628aa58
egort <egortolkachev2016@gmail.com>
2023-10-27 11:22:10 +0300

    Game loop

--- app/src/main/java/com/example/androidstudio2dgame/MainActivity.java
+++ app/src/main/java/com/example/androidstudio2dgame/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.androidstudio2dgame;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+/**
+ * MainActivity is the entry point to our application
+ * Точка входа в наше приложение
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Set window to fullscreen
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //Set content view to game, so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
+
+
+
     }
 }
